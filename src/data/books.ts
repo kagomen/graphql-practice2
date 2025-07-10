90ba3e651b42b31dbd704e751c14fb3e3b3d3fe6
kagomen <fu.taz.1102@gmail.com>
2025-07-10 17:13:28 +0900

    add simple feature

---
+++ src/data/books.ts
@@ -0,0 +1,10 @@
+export const books = [
+  {
+    title: "吾輩は猫である",
+    author: "夏目漱石",
+  },
+  {
+    title: "走れメロス",
+    author: "太宰治",
+  },
+]
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log("hello")
+import { ApolloServer, gql } from "apollo-server"
+import { books } from "./data/books"
+
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+server.listen().then(({ url }) => {
+  console.log(`server listen : ${url}`)
+})
