a59b5a630840b87aa897af8835f1cc229cf10ccf
kagomen <fu.taz.1102@gmail.com>
2025-07-11 16:42:26 +0900

    display tasks

--- react-client/src/App.tsx
+++ react-client/src/App.tsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+interface Task {
+  id: string
+  title: string
+  completed: boolean
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const tasks: Task[] = [
+    { id: "1", title: "掃除", completed: false },
+    { id: "2", title: "洗濯", completed: false },
+  ]
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <form action="">
+        <input type="text" placeholder="Add Task" />
+        <button>+</button>
+      </form>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            <input type="checkbox" checked={task.completed} />
+            {task.title}
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
